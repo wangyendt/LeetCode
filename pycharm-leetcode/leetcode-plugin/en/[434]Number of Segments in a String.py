8e8e811bd7e68e1120aff0fea41dece9f14faa5d
wangyendt <905317742@qq.com>
2019-06-14 01:17:04 +0900

    add easy 434

---
+++ pycharm-leetcode/leetcode-plugin/en/[434]Number of Segments in a String.py
@@ -0,0 +1,14 @@
+# Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+#
+# Please note that the string does not contain any non-printable characters. 
+#
+# Example: 
+# 
+# Input: "Hello, my name is John"
+# Output: 5
+# 
+#
+
+class Solution:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
