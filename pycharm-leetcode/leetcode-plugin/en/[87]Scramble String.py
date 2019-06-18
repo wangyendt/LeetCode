5db193e0d7c966cfb04fa936b904e924cf4829d5
wangyendt <905317742@qq.com>
2019-06-18 18:58:41 +0900

    rearrange files

---
+++ pycharm-leetcode/leetcode-plugin/en/[87]Scramble String.py
@@ -0,0 +1,74 @@
+# Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+#
+# Below is one possible representation of s1 = "great": 
+#
+# 
+#    great
+#   /    \
+#  gr    eat
+# / \    /  \
+# g   r  e   at
+#           / \
+#          a   t
+# 
+#
+# To scramble the string, we may choose any non-leaf node and swap its two children. 
+#
+# For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat". 
+#
+# 
+#    rgeat
+#   /    \
+#  rg    eat
+# / \    /  \
+# r   g  e   at
+#           / \
+#          a   t
+# 
+#
+# We say that "rgeat" is a scrambled string of "great". 
+#
+# Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae". 
+#
+# 
+#    rgtae
+#   /    \
+#  rg    tae
+# / \    /  \
+# r   g  ta  e
+#       / \
+#      t   a
+# 
+#
+# We say that "rgtae" is a scrambled string of "great". 
+#
+# Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1. 
+#
+# Example 1: 
+#
+# 
+# Input: s1 = "great", s2 = "rgeat"
+# Output: true
+# 
+#
+# Example 2: 
+#
+# 
+# Input: s1 = "abcde", s2 = "caebd"
+# Output: false
+#
+
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) == 1:
+            return s2 == s1
+        p, end = 0, 0
+        dic = {}
+        while p <= end < len(s2):
+            if not dic.get(s2[p]):
+                
+
+
+so = Solution()
+print(so.isScramble('great', 'rgeat'))
+print(so.isScramble('greart', 'rrgeat'))
