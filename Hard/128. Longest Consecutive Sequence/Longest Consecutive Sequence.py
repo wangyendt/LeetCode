f5dc287bbc873a3703c59cdcce0cc576295a79fa
Ye Wang <905317742@qq.com>
2019-07-02 23:52:29 +0800

    add hard 128

---
+++ Hard/128. Longest Consecutive Sequence/Longest Consecutive Sequence.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+""" 
+@author:  wangye
+@file: Longest Consecutive Sequence.py 
+@time: 2019/07/02
+@contact: wangye.hope@gmail.com
+@site:  
+@software: PyCharm 
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def longestConsecutive(self, nums: list) -> int:
+        if not nums:
+            return 0
+        nums = list(set(nums))
+        nums.sort()
+        n = len(nums)
+        dp = [1] * (n + 1)
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] == 1:
+                dp[i] = dp[i - 1] + 1
+        return max(dp)
