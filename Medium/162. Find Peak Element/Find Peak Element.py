48e6663fb0f8a305c18a9842518c08bb91728b22
wangyendt <905317742@qq.com>
2019-08-26 14:55:04 +0800

    add medium 162

--- Medium/162. Find Peak Element/Find Peak Element.py
+++ Medium/162. Find Peak Element/Find Peak Element.py
@@ -12,4 +12,15 @@
 
 class Solution:
     def findPeakElement(self, nums: list) -> int:
-        
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] > nums[m + 1]:
+                r = m
+            else:
+                l = m + 1
+        return l
+
+
+so = Solution()
+print(so.findPeakElement([1, 2, 3, 1]))
