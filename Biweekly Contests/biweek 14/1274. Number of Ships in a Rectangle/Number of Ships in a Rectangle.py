4fb1b7fc5fe59f7ce9e3b8fe472e2afa50b8b0a7
Ye Wang <905317742@qq.com>
2019-12-02 16:42:51 +0800

    Update Number of Ships in a Rectangle.py

--- Biweekly Contests/biweek 14/1274. Number of Ships in a Rectangle/Number of Ships in a Rectangle.py
+++ Biweekly Contests/biweek 14/1274. Number of Ships in a Rectangle/Number of Ships in a Rectangle.py
@@ -11,4 +11,14 @@
 
 
 class Solution(object):
-    def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
+    def countShips(self, sea: 'Sea', tr: 'Point', bl: 'Point') -> int:
+        ret = 0
+        if tr.x >= bl.x and tr.y >= bl.y and sea.hasShips(tr, bl):
+            if tr.x == bl.x and tr.y == bl.y:
+                return 1
+            mx, my = (tr.x + bl.x) // 2, (tr.y + bl.y) // 2
+            ret += self.countShips(sea, tr, Point(mx + 1, my + 1))
+            ret += self.countShips(sea, Point(mx, my), bl)
+            ret += self.countShips(sea, Point(mx, tr.y), Point(bl.x, my + 1))
+            ret += self.countShips(sea, Point(tr.x, my), Point(mx + 1, bl.y))
+        return ret
