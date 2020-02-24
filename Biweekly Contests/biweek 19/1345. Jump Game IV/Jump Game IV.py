8cbbe189d76d6f1fc4f6effe255d3d942249402f
Ye Wang <905317742@qq.com>
2020-02-24 13:55:32 +0800

    add part of biweekly contest 19

---
+++ Biweekly Contests/biweek 19/1345. Jump Game IV/Jump Game IV.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：LeetCode -> Jump Game IV
+@IDE    ：PyCharm
+@Author ：Wang Ye (Wayne)
+@Date   ：2020/2/22 3:34
+@Desc   ：
+=================================================="""
+
+
+class Solution:
+    def minJumps(self, arr: list) -> int:
+
