384ba352c6e3dbdf0616b2af7dd2a5b8281181fb
wangyendt <905317742@qq.com>
2020-02-24 18:13:15 +0800

    add biweekly contest 19

--- Biweekly Contests/biweek 19/1345. Jump Game IV/Jump Game IV.py
+++ Biweekly Contests/biweek 19/1345. Jump Game IV/Jump Game IV.py
@@ -8,7 +8,26 @@
 @Desc   ：
 =================================================="""
 
+import collections
+import itertools
+
 
 class Solution:
-    def minJumps(self, arr: list) -> int:
+    def minJumps(self, A: list) -> int:
+        indices = collections.defaultdict(list)
+        for i, a in enumerate(A):
+            indices[a].append(i)
+        done, now = {-1}, {0}
+        for steps in itertools.count():
+            done |= now
+            if len(A) - 1 in done:
+                return steps
+            now = {j for i in now for j in [i - 1, i + 1] + indices.pop(A[i], [])} - done
 
+
+so = Solution()
+print(so.minJumps(A=[100, -23, -23, 404, 100, 23, 23, 23, 3, 404]))
+print(so.minJumps(A=[7]))
+print(so.minJumps(A=[7, 6, 9, 6, 9, 6, 9, 7]))
+print(so.minJumps(A=[6, 1, 9]))
+print(so.minJumps(A=[11, 22, 7, 7, 7, 7, 7, 7, 7, 22, 13]))
