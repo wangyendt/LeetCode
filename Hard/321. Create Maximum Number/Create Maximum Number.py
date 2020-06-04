bfc9cc1df19c9350b3d891f832c6094880449b62
Ye Wang <905317742@qq.com>
2020-06-04 21:09:43 +0800

    todo: hard 321

---
+++ Hard/321. Create Maximum Number/Create Maximum Number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Wayne
+@contact: wangye.hope@gmail.com
+@software: PyCharm
+@file: Create Maximum Number
+@time: 2020/6/4 19:30
+"""
+
+
+class Solution:
+    def maxNumber(self, nums1: list, nums2: list, k: int) -> list:
+        
