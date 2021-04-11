a66650a4c789b0f4d43810a2e2615813d3506e97
Ye Wang <905317742@qq.com>
2021-04-12 02:17:40 +0800

    Update API_utils.py

--- Tools/API_utils.py
+++ Tools/API_utils.py
@@ -10,6 +10,26 @@
 """
 
 
+class DataStructureProblem:
+    def __init__(self, p_order, p_params):
+        self.order = p_order
+        self.params = p_params
+
+    def get_result(self):
+        assert len(self.order) == len(self.params) > 0
+        obj = eval(f'{self.order[0]}(*{self.params[0]})')
+        for i, (o, p) in enumerate(zip(self.order, self.params)):
+            if i > 0:
+                yield getattr(obj, o)(*p)
+
+    def print(self):
+        for res in self.get_result():
+            if res:
+                print(res, end=',')
+            else:
+                print('null', end=',')
+
+
 def call_me(class_, callers, params):
     cls = None
     my_class = class_
@@ -17,4 +37,6 @@ def call_me(class_, callers, params):
         if func == my_class.__name__:
             cls = my_class(*param)
         else:
-            print(cls.__getattribute__(func)(*param), end=' ')
+            res = cls.__getattribute__(func)(*param)
+            prt = res if res else 'null'
+            print(prt, end=',')
