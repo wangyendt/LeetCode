fc26266836c96a44ee8c3c1460748002f5b6d93f
80301815 <905317742@qq.com>
2022-04-18 21:34:25 +0800

    add part of contest week 289 -- No.2

---
+++ Contests/week 289/2246. Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+""" 
+@author: Wang Ye (Wayne)
+@file: Longest Path With Different Adjacent Characters.py
+@time: 2022/04/18
+@contact: wangye@oppo.com
+@site: 
+@software: PyCharm
+# code is far away from bugs.
+"""
+
+
+
+from typing import *
+
+
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        pass
