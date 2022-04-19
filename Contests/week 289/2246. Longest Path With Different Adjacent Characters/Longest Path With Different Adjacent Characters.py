a0982fe510c3b52c96bae0309482668b4e7d4003
80301815 <905317742@qq.com>
2022-04-19 19:28:30 +0800

    add contest week 289

--- Contests/week 289/2246. Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.py
+++ Contests/week 289/2246. Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.py
@@ -10,11 +10,37 @@
 # code is far away from bugs.
 """
 
-
-
+import collections
+import heapq
 from typing import *
 
 
 class Solution:
     def longestPath(self, parent: List[int], s: str) -> int:
-        pass
+        res = collections.defaultdict(list)
+        for i, p in enumerate(parent):
+            if p >= 0:
+                res[p].append(i)
+
+        ret = [0]
+
+        def helper(i):
+            candi = [0]
+            for j in res[i]:
+                cur = helper(j)
+                if s[i] != s[j]:
+                    candi.append(cur)
+
+            candi = heapq.nlargest(2, candi)
+            ret[0] = max(ret[0], sum(candi) + 1)
+            return max(candi) + 1
+
+        helper(0)
+        return ret[0]
+
+
+so = Solution()
+print(so.longestPath(parent=[-1, 0, 0, 0], s="aabc"))
+print(so.longestPath([-1, 0, 0, 1, 1, 2], "abacbe"))
+print(so.longestPath([-1], "z"))
+print(so.longestPath([-1, 0, 0], "aba"))
