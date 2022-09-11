728b5b4ef3dd839b7b3fa4f38c4f9aaae1a32de1
Ye Wang <905317742@qq.com>
2022-09-11 13:26:47 +0800

    add max segment tree

--- Tools/SegmentTree.py
+++ Tools/SegmentTree.py
@@ -122,3 +122,31 @@ class SegmentTreeIterative:
         for x in self.d:
             ans = self.F(ans, self.sums[x])
         return ans
+
+
+class SEG:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * 2 * self.n
+
+    def query(self, l, r):
+        l += self.n
+        r += self.n
+        ans = 0
+        while l < r:
+            if l & 1:
+                ans = max(ans, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                ans = max(ans, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return ans
+
+    def update(self, i, val):
+        i += self.n
+        self.tree[i] = val
+        while i > 1:
+            i >>= 1
+            self.tree[i] = max(self.tree[i * 2], self.tree[i * 2 + 1])
